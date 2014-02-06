ee143528e2c6f3fa8a853e1ec1d563153d3b674e
Zdenek Pizl <zdenek.pizl@gooddata.com>
2014-02-07 00:05:30 +0100

    FEATURE: prepare possibility to use different renderers within multitarget graph
    - must use renderer: multi in graph
    - must define particular renderer: within a target
    - target's alias must match similarly as for color:
    
    - newest Rickshaw and d3 is used

--- dashboards.js
+++ dashboards.js
@@ -1,180 +1,99 @@
-var graphite_url = "demo";  // enter your graphite url, e.g. http://your.graphite.com
+var graphite_url = "http://zpi-sandbox.na.getgooddata.com";  // enter your graphite url, e.g. http://your.graphite.com
 
 var dashboards = 
 [
-  { "name": "Demo",  // give your dashboard a name (required!)
-    "refresh": 5000,  // each dashboard has its own refresh interval (in ms)
-    // add an (optional) dashboard description. description can be written in markdown / html.
-    "description": "#Giraffe : A [Graphite](http://graphite.wikidot.com) Dashboard with a long neck <img class='pull-right' src='img/giraffe.png' />"
-                +"\n"
-                +"\n<br/>"
-                +"\n<a class='btn btn-large btn-warning' href='https://github.com/kenhub/giraffe'><i class='icon-github icon-large'></i> View project on github</a>"
-                +"\n"
-                +"\n##More?"
-                +"\n"
-                +"\nCheck out the different dashboards for more information about riding giraffes."
-                ,
-    "metrics":  // metrics is an array of charts on the dashboard
-    [
-      {
-        "alias": "signups",  // display name for this metric
-        "target": "sumSeries(enter.your.graphite.metrics.here)",  // enter your graphite barebone target expression here
-        "description": "New signups to the website",  // enter your metric description here
-        "summary": "sum",  // available options: [sum|min|max|avg|last|<function>]
-        "summary_formatter": d3.format(",f") // customize your summary format function. see d3 formatting docs for more options
-        // also supported are tick_formatter to customize y axis ticks, and totals_formatter to format the values in the legend
-      },
-      {
-        "alias": "signup breakdown",
-        "targets": ["sumSeries(enter.your.graphite.metrics.here)",  // targets array is also supported
-                    "sumSeries(enter.another.graphite.metrics)"],   // see below for more advanced usage
-        "description": "signup breakdown based on site location",
-        "renderer": "area",  // use any rickshaw-supported renderer
-        "unstack": true  // other parameters like unstack, interpolation, stroke, min, height are also available (see rickshaw documentation for more info)
-      },
-      {
-        "alias": "Registration breakdown",
-        "target": "sumSeries(enter.your.graphite.metrics.here)", 
-        // target can use a javascript function. This allows using dynamic parameters (e.g. period). See a few functions
-        // at the bottom of this file.
-        "target": function() { return 'summarize(events.registration.success,"' + entire_period() + 'min)' },
-        "renderer": "bar",
-        "description": "Registrations based on channel",
-        "hover_formatter": d3.format("03.6g"),  // customize your hover format
-        "null_as": 0  // null values are normally ignored, but you can convert null to a specific value (usually zero)
-      },
-    ]
-  },
-  { "name": "Visuals",
+
+  /* This is a block for Dashboard */
+  { "name": "Giraffe PoC",
     "refresh": 10000,
-    // you can use any rickshaw supported color scheme.
-    // Enter palette name as string, or an array of colors
-    // (see var scheme at the bottom).
-    // Schemes can be configured globally (see below), per-dashboard, or per-metric
     "scheme": "classic9",   // this is a dashboard-specific color palette
-    "description": "#Visual settings <img class='pull-right' src='img/giraffe.png' />"
-                +"\n"
-                +"\nGiraffe is using the [Rickshaw](http://code.shutterstock.com/rickshaw/) d3 charting library."
-                +"\nYou can therefore configure almost any visual aspect supported by rickshaw/d3 inside giraffe."
-                +"\n"
-                +"\nThis includes: [color scheme](https://github.com/shutterstock/rickshaw#rickshawcolorpalette), interpolation, [renderer](https://github.com/shutterstock/rickshaw#renderer) and more."
-                +"\n"
-                +"\nEach graph can span over 1,2 or 3 columns using the `colspan` metric parameter."
-                +"\n"
-                +"\n##Top panel"
-                +"\n"
-                +"\nThe top panel allows toggling between time ranges (not visible on the demo, but should work fine with graphite)."
-                +"\nIt also supports toggling the legend <i class='icon-list-alt'></i>, grid <i class='icon-th'></i>"
-                +"\n, X labels <i class='icon-comment'></i> and tooltips <i class='icon-remove-circle'></i>"
-                +"\n"
-                +"\n##<i class='icon-list-alt'></i> Legend"
-                +"\n"
-                +"\nClicking on the legend will show the legend under each chart. The legend includes summary information for each series, "
-                +"\n &Sigma;: Total; <i class='icon-caret-down'></i>: Minimum; <i class='icon-caret-up'></i>: Maximum; and <i class='icon-sort'></i>: Average"
-                +"\n"
-                +"\n##Summary"
-                +"\n"
-                +"\nIn addition to the legend, each chart can have one `summary` value displayed next to its title."
-                +"\nThe summary is calculated over the entire series and can be one of `[sum|max|min|avg|last|<function>]`"
-                +"\n"
-                +"\n"
-                +"\n"
-                ,
+    "description": "",
     "metrics": 
+  /* This is a block for only one Graph */
     [
       {
-        "alias": "network",
-        "target": "aliasByNode(derivative(servers.system.eth*),4)",
+        "alias": "Open files used",
+        "target": ["alias(avg(servers.stg-pdwh0[123].system.open_files.used),'Average of stg-pdwh0[123]')",
+    		    "aliasByNode(servers.stg-pdwh0[123].system.open_files.used,1)"],
         "events": "*",  // instead of annotator, if you use the graphite events feature
                         // you can retrieve events matching specific tag(s) -- space separated
                         // or use * for all tags. Note you cannot use both annotator and events.
-        "description": "main system cpu usage on production (cardinal interpolation, line renderer, colspan=3)",
+        "description": "Open files used",
+    	"renderer": "line",
         "interpolation": "linear",
         "colspan": 3,
       },
+    ]
+  },
+
+  /* This is a block for one whole Dashboard */
+  {
+    "name": "DEV-CMP06 characteristics",
+    "refresh": 30000,
+    "scheme": "classic9",   // this is a dashboard-specific color palette
+    "description": "",
+    "metrics":
+  /* This is a block for set of 3 Graphs */
+    [
+      /* Graph 0 */
       {
-        "alias": "cpu utilization",
-        "target": "aliasByNode(derivative(servers.system.cpu.*),4)",  // target can use any graphite-supported wildcards
-        "annotator": 'events.deployment',  // a simple annotator will track a graphite event and mark it as 'deployment'.
-                                           // enter your graphite target as a string
-        "description": "cpu utilization on production (using linear interpolation). Summary displays the average across all series",
-        "interpolation": "linear",  // you can use different rickshaw interpolation values
-        "stroke_width": 1,  // change stroke width
-        "stroke": stroke,  // stoke may be true, false or a function that takes and returns a d3 rgb color to style the stroke
-        "summary": "avg",
-        "totals_fields": ["min", "max", "avg"],  // customize which totals are shown in the legend
-        "summary_formatter": format_pct,
-        "hover_formatter": format_pct,
-        "totals_formatter": format_pct,  // customize the formatting of the legend totals
-        "tick_formatter": format_pct,  // and also the y axis ticks
-      },
-      {
-        "alias": "proc mem prod",
-        "targets": ["aliasByNode(derivative(servers.system.cpu.user),4)",  // targets array can include strings, 
-                                                                           // functions or dictionaries
-                   {target: 'alias(derivative(servers.system.cpu.system,"system utilization")',
-                    alias: 'system utilization',                           // if you use a graphite alias, specify it here
-                    color: '#f00'}],                                       // you can also specify a target color this way
-                                                                           // (note that these values are ignored on the demo)
-        // annotator can also be a dictionary of target and description.
-        // However, only one annotator is supported per-metric.
-        "annotator": {'target' : 'events.deployment',
-                      'description' : 'deploy'},
-        "description": "main process memory usage on production (different colour scheme and interpolation)",
-        "interpolation": "step-before",
-        "scheme": "munin",  // this is a metric-specific color palette
-      },
-      {
-        "alias": "sys mem prod",
-        "target": "aliasByNode(derivative(servers.system.cpu.*),4)",
+        "alias": "System Load",
+        "targets": [
+            {'target': 'aliasByNode(servers.dev-cmp06.system.load.load,1)',
+                'color': '#a00', 'renderer': 'bar', 'alias': 'dev-cmp06'},
+            {'target': 'alias(movingAverage(servers.dev-cmp06.system.load.load,5),' +
+                '"Moving Average of 5 segments")', 'color': '#F00', 'alias': 'Moving Average of 5 segments',
+                'renderer': 'line'}
+        ],
+
         "events": "*",  // instead of annotator, if you use the graphite events feature
                         // you can retrieve events matching specific tag(s) -- space separated
                         // or use * for all tags. Note you cannot use both annotator and events.
-        "description": "main system memory usage on production (cardinal interpolation, line renderer)",
-        "interpolation": "cardinal",
-        "renderer": "line",
-        "max": 150,  // you can specify max value for the y-axis
-        "min": 20,   // and also min
+                        
+        "description": "",
+        "interpolation": "linear",
+        "renderer": 'multi',
+        "colspan": 2,
       },
-    ]
-  },
-  { "name": "Setup",
-    "refresh": 10000,
-    "scheme": "colorwheel",
-    "graphite_url": "demo",  // you can override the default graphite_url with a dashboard-specific url
-    "description": "#Setup and configuration <img class='pull-right' src='img/giraffe.png' />"
-                +"\n"
-                +"\n##Installation"
-                +"\n"
-                +"\nTo install giraffe, simply [download](https://github.com/kenhub/giraffe/archive/master.zip) the code and run it from your browser."
-                +"\nYou can put it on any type of web server, and also open the `index.html` file from your local drive."
-                +"\n"
-                +"\n##Authentication"
-                +"\n"
-                +"\nGiraffe uses JSONP to retrieve the data from your graphite server. It should work out of the box, unless you"
-                +"\nhave setup authentication. Basic authentication seems to work in Firefox (it will prompt you), "
-                +"\nbut with Chrome you might need to authenticate to your graphite server first, and then access Giraffe."
-                +"\n"
-                +"\n##Configuration"
-                +"\n"
-                +"\nThe main configuration for all dashboards is found in `dashboards.js`. The file is reasonably self-explanatory, "
-                +"\nso please take a look."
-                +"\n"
-                +"\nIf you need to change the page layout, CSS, or add/remove a time period, you can also edit `index.html` and `css/main.css` file."
-                +"\n"
-                ,
-    "metrics": 
-    [
+
+      /* Graph 1 */
       {
-        "alias": "production HTTP req",
-        "target": "aliasByNode(derivative(servers.gluteus-medius.Http.http_response_rates.*),4)",
-        "renderer": "bar",
-        "interpolation": "cardinal",
-        "summary": "last",
+        "alias": "Processes",
+
+        "targets": 'servers.dev-cmp06.processes.processes.processes',
+
+        "events": "*",  // instead of annotator, if you use the graphite events feature
+                        // you can retrieve events matching specific tag(s) -- space separated
+                        // or use * for all tags. Note you cannot use both annotator and events.
+                        
+        "description": "",
+        "renderer": "area",
+        "stroke": stroke,
+        "unstack": true,
+        "interpolation": "linear",
+        "colspan": 1,
+      },
+
+      /* Graph 2 */
+      {
+        "alias": "Disks utilization",
+
+        "targets": 'aliasByNode(servers.dev-cmp06.disk.diskstats.diskstats_utilization.sd*_util,5)',
+
+        "events": "*",  // instead of annotator, if you use the graphite events feature
+                        // you can retrieve events matching specific tag(s) -- space separated
+                        // or use * for all tags. Note you cannot use both annotator and events.
+                        
+        "description": "",
+        "renderer": "line",
+        "unstack": true,
+        "interpolation": "linear",
+        "colspan": 2,
       },
     ]
   },
 ];
+
 
 var scheme = [
               '#423d4f',
